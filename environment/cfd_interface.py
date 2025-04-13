bbd46341ca4946d642c8270d30e334b3b0b30d9d
taraldkb <97185071+taraldkb@users.noreply.github.com>
2025-04-13 12:56:35 +0200

    Update cfd_interface.py
    Fix syntax mistake

--- environment/cfd_interface.py
+++ environment/cfd_interface.py
@@ -25,7 +25,7 @@ def run_cfd_step(solver, current_state, action, design_params, report_path, wate
 
     # Set boundary conditions
     try:
-        solver.setup.boundary_conditions.velocity_inlet["wind"].momentum.value = map_value(wind_velocity(1.0, 20.0))
+        solver.setup.boundary_conditions.velocity_inlet["wind"].momentum.value = map_value(wind_velocity,1.0, 20.0)
 
     except Exception as e:
         print(f"[WARNING] Could not set wind velocity: {e}")
