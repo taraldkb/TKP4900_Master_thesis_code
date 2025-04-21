3dc6712b527507dc870265e24e0feb525cddd080
taraldkb <bishoptarald@gmail.com>
2025-04-21 18:30:59 +0200

    create new function and main setup

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from rl.train import train_agent
+from rl.train import train_agent, continue_train_agent
 from utils.cleanup import cleanup
 import os
-train_agent()
-#cleanup()
+
+choice = int(input("what to run"))
+
+if choice == 0:
+    cleanup()
+elif choice == 1:
+    train_agent()
+elif choice == 2:
+    continue_train_agent("Save/best_policy.pt", 100)
+else:
+    print("nope")
 
