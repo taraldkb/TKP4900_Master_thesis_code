01f54fc9892e5d016e354876498c49f12550a656
Taraldkb <bishoptarald@gmail.com>
2025-04-29 10:48:08 +0200

    create new train agent logs

--- rl/train.py
+++ rl/train.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 import torch
@@ -11,7 +12,7 @@ from environment.water_injection_env import WaterInjectionEnv
 from environment.cfd_interface import run_cfd_step
 from utils.map_value_function import *
 from utils.read_report_function import *
-import matplotlib.pyplot as plt
+from datetime import date
 
 # Load config
 with open("configs/RL_config.json", "r") as f:
@@ -69,8 +70,12 @@ def normalize(x):
 
 
 # create Train agent
-def train_agent():
-    writer = SummaryWriter(log_dir=CONFIG["log_dir"])
+def train_agent(log_name = None):
+
+    if log_name is None:
+        log_name = str(date.today())
+    log_name = log_name + ".csv"
+    log_file = os.path.join(CONFIG['log_dir'], log_name)
 
     env = WaterInjectionEnv(run_cfd_step)
     policy = PolicyNet(CONFIG["state_dim"], CONFIG["action_dim"], CONFIG["hidden_size"])
@@ -81,62 +86,60 @@ def train_agent():
 
     best_reward = -float("inf")
 
-    for episode in range(CONFIG["epochs"]):
-        state = env.reset()
-        log_probs, rewards, values = [], [], []
-        done = False
-        total_reward = 0
+    with open(log_file, mode="w", newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["episode", "reward", "policy_loss","value_loss"])
 
-        while not done:
-            state_tensor = torch.tensor(state, dtype=torch.float32)
-            mean, std = policy(state_tensor)
-            dist = Normal(mean, std)
-            action = dist.sample()
-            if CONFIG["clip_actions"]:
-                action = action.clamp(0, 1)
+        for episode in range(CONFIG["epochs"]):
+            state = env.reset()
+            log_probs, rewards, values = [], [], []
+            done = False
+            total_reward = 0
 
-            log_prob = dist.log_prob(action).sum()
-            value_est = value(state_tensor)
+            while not done:
+                state_tensor = torch.tensor(state, dtype=torch.float32)
+                mean, std = policy(state_tensor)
+                dist = Normal(mean, std)
+                action = dist.sample()
+                if CONFIG["clip_actions"]:
+                    action = action.clamp(0, 1)
 
-            next_state, reward, done, _ = env.step(action.detach().numpy())
+                log_prob = dist.log_prob(action).sum()
+                value_est = value(state_tensor)
 
-            log_probs.append(log_prob)
-            rewards.append(reward)
-            values.append(value_est)
-            state = next_state
-            total_reward += reward
+                next_state, reward, done, _ = env.step(action.detach().numpy())
 
-        returns = compute_returns(rewards, CONFIG["gamma"])
-        log_probs = torch.stack(log_probs)
-        values = torch.stack(values)
-        advantages = normalize(returns - values.detach())
+                log_probs.append(log_prob)
+                rewards.append(reward)
+                values.append(value_est)
+                state = next_state
+                total_reward += reward
 
-        policy_loss = -(log_probs * advantages).sum()
-        optimizer_policy.zero_grad()
-        policy_loss.backward()
-        optimizer_policy.step()
+            returns = compute_returns(rewards, CONFIG["gamma"])
+            log_probs = torch.stack(log_probs)
+            values = torch.stack(values)
+            advantages = normalize(returns - values.detach())
 
-        value_loss = nn.functional.mse_loss(values, returns)
-        optimizer_value.zero_grad()
-        value_loss.backward()
-        optimizer_value.step()
+            policy_loss = -(log_probs * advantages).sum()
+            optimizer_policy.zero_grad()
+            policy_loss.backward()
+            optimizer_policy.step()
 
-        writer.add_scalar("reward", total_reward, episode)
-        writer.add_scalar("loss/policy", policy_loss.item(), episode)
-        writer.add_scalar("loss/value", value_loss.item(), episode)
+            value_loss = nn.functional.mse_loss(values, returns)
+            optimizer_value.zero_grad()
+            value_loss.backward()
+            optimizer_value.step()
 
-        print(
-            f"Ep {episode + 1}: Reward={total_reward:.2f}, PolicyLoss={policy_loss.item():.3f}, "
-            f"ValueLoss={value_loss.item():.3f}")
+            csv_writer.writerow([episode, total_reward, policy_loss.item(), value_loss.item()])
 
-        if total_reward > best_reward:
-            best_reward = total_reward
-            torch.save(policy.state_dict(), CONFIG["save_path"])
-    writer.close()
+            if total_reward > best_reward:
+                best_reward = total_reward
+                torch.save(policy.state_dict(), CONFIG["save_path"])
+
     print("Training complete. Best reward:", best_reward)
 
 
-def continue_train_agent(policy_path, episodes=100):
+def continue_train_agent(policy_path, log_path, episodes=100):
     writer = SummaryWriter(log_dir=CONFIG["log_dir"])
 
     env = WaterInjectionEnv(run_cfd_step)
