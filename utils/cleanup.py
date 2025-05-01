61c5c5e9b28cf6764e5cb7cd3173c5a91e5653d3
taraldkb <bishoptarald@gmail.com>
2025-05-01 10:47:48 +0200

    fix cleanup function

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -1,27 +1,25 @@
 import os
 import time
 
+
 def cleanup(report_path="concentration.out", loss_report_path="water_loss.out", water_usage_report_path="water_usage.out" ):
-    if os.path.exists(report_path):
-        for _ in range(10):
-            try:
-                os.remove(report_path)
-                break
-            except PermissionError:
-                time.sleep(0.5)
+    while os.path.exists(report_path):
+        try:
+            os.remove(report_path)
+            break
+        except PermissionError:
+            time.sleep(0.5)
 
-    if os.path.exists(water_usage_report_path):
-        for _ in range(10):
-            try:
-                os.remove(water_usage_report_path)
-                break
-            except PermissionError:
-                time.sleep(0.5)
+    while os.path.exists(water_usage_report_path):
+        try:
+            os.remove(water_usage_report_path)
+            break
+        except PermissionError:
+            time.sleep(0.5)
 
-    if os.path.exists(loss_report_path):
-        for _ in range(10):
-            try:
-                os.remove(loss_report_path)
-                break
-            except PermissionError:
-                time.sleep(0.5)
+    while os.path.exists(loss_report_path):
+        try:
+            os.remove(loss_report_path)
+            break
+        except PermissionError:
+            time.sleep(0.5)
