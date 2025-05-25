1f2cbd21d95745ad760d6e96902261fec8878faf
Taraldkb <bishoptarald@gmail.com>
2025-05-25 20:14:07 +0200

    ad ability to run multiple tests

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from rl.train import train_agent, continue_train_agent
 from rl.test import test_agent
 from utils.cleanup import cleanup
 from utils.plot_logs_function import plot_logs
-import os
+from rl.run_test import test_multiple
+
 
 choice = int(input("what to run: "))
 
@@ -14,13 +15,15 @@ elif choice == 1:
 elif choice == 2:
     case = input("which case: ")
     ep = int(input("how many episodes: "))
-    continue_train_agent("Save/Case"+case+".pt", "logs/case"+case+".csv", case, ep)
+    continue_train_agent("Save/Case"+case+".pt", "logs/case"+case+".csv", int(case), ep)
 elif choice == 3:
     case = input("which case: ")
     plot_logs("logs/case"+case+".csv")
 elif choice == 4:
     case = input("which case: ")
-    test_agent(case, "Save/Case"+case+".pt")
+    test_agent(int(case), "Save/Case"+case+".pt")
+elif choice == 5:
+    test_multiple()
 else:
     print("nope")
 
---
+++ rl/run_test.py
@@ -0,0 +1,9 @@
+from rl.test import test_agent
+
+
+
+def test_multiple():
+    test_cases = [3, 5, 6 ]
+
+    for i in test_cases:
+        test_agent(test_agent(i, f"Save/Case{i}.pt"))
