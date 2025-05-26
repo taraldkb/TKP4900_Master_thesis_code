cba075df8f98200e29bbf44622e7087c83df287f
Taraldkb <bishoptarald@gmail.com>
2025-05-26 09:10:40 +0200

    update

--- rl/run_test.py
+++ rl/run_test.py
@@ -3,7 +3,7 @@ from rl.test import test_agent
 
 
 def test_multiple():
-    test_cases = [3, 5, 6 ]
+    test_cases = [ 5, 6 ]
 
     for i in test_cases:
         test_agent(i, f"Save/Case{i}.pt")
